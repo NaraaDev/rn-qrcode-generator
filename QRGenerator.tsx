7f2fc218906c94f2631118f8893b4e866d8b9f2d
Massive Naraa <massive0304@gmail.com>
2022-07-05 12:45:35 +0800

    fix: change file name

---
+++ QRGenerator.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { StyleProp, View, ViewStyle, StyleSheet } from "react-native";
+
+import { QRCodeErrorCorrectionLevel } from "qrcode";
+
+import Matrix from "./Matrix";
+
+export function QRGenerator({
+  bgColor = "white",
+  fgColor = "black",
+  value,
+  ecl,
+  style,
+}: {
+  value: string;
+  ecl: QRCodeErrorCorrectionLevel;
+  style: StyleProp<ViewStyle>;
+  bgColor?: string;
+  fgColor?: string;
+}) {
+  const arr = Matrix({ value: value, errorCorrectionLevel: ecl });
+  const bgBLack = fgColor;
+  const bgWhite = bgColor;
+
+  return (
+    <View style={style}>
+      {arr.map((item: Array<number>, rowIndex: number) => {
+        return (
+          <View style={[styles.row, styles.flex]} key={rowIndex}>
+            {item.map((_, index) => {
+              return (
+                <View
+                  style={[
+                    styles.flex,
+                    {
+                      backgroundColor: item[index] === 1 ? bgBLack : bgWhite,
+                    },
+                  ]}
+                />
+              );
+            })}
+          </View>
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  row: { flexDirection: "row" },
+  flex: { flex: 1 },
+  view: {
+    position: "absolute",
+    backgroundColor: "red",
+  },
+});
+
+module.exports = QRGenerator;
